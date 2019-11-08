24fdda4e5174b06321e77b8bd9dc12787573b263
Saad Baradan <saadbaradan@gmail.com>
2019-11-08 10:25:07 -0700

    Write function that displays result of all operators

--- Calculator/app/src/main/java/com/onxmaps/playground/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/onxmaps/playground/calculator/MainActivity.kt
@@ -8,10 +8,18 @@ import android.view.View
 import android.widget.Toast
 import android.widget.Button
 import android.widget.TextView
+import android.util.Log
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import timber.log.Timber
+
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,7 +33,11 @@ class MainActivity : AppCompatActivity() {
      * extra time, keep and display a history of all operations done.
      * Use HistoryAdapter for the display
      */
-      lateinit var display: TextView
+       lateinit var display: TextView
+       var num1 = ""
+       var num2 = ""
+       var operator = ""
+       var isOperatorClicked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,15 +67,82 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun displayOperator(view: View) {
+        isOperatorClicked = true
         if( display.text.contains("x") ||
             display.text.contains("+") ||
             display.text.contains("-") ||
-            display.text.contains("/"))
+            display.text.contains("/") ||
+            display.text.contains("%"))
         {
             return;
         } else {
             display.append((view as Button).text)
         }
+    }
+
+    fun evaluateExpression(view: View) {
+        var expression = display.getText().toString()
+        if (expression.contains("/")) {
+            divisionEvaluation()
+        } else if(expression.contains("x")) {
+            productEvaluation()
+        } else if(expression.contains("+")) {
+            sumEvaluation()
+        } else if(expression.contains("-")) {
+            differenceEvaluation()
+        } else if(expression.contains("%")) {
+            modulusEvaluation()
+        }
+    }
+
+     fun divisionEvaluation() {
+         var expression = display.getText().toString()
+         var numList = expression.split("/")
+         var firstNumber = numList.toMutableList().removeAt(0)
+         var secondNumber = numList.toMutableList().removeAt(1)
+         var result = firstNumber.toFloat() / secondNumber.toFloat()
+         var stringResult = result.toString()
+         display.text = stringResult
+     }
+
+     fun productEvaluation() {
+         var expression = display.getText().toString()
+         var numList = expression.split("x")
+         var firstNumber = numList.toMutableList().removeAt(0)
+         var secondNumber = numList.toMutableList().removeAt(1)
+         var result = firstNumber.toFloat() * secondNumber.toFloat()
+         var stringResult = result.toString()
+         display.text = stringResult
+    }
+
+    fun sumEvaluation() {
+        var expression = display.getText().toString()
+        var numList = expression.split("+")
+        var firstNumber = numList.toMutableList().removeAt(0)
+        var secondNumber = numList.toMutableList().removeAt(1)
+        var result = firstNumber.toFloat() + secondNumber.toFloat()
+        var stringResult = result.toString()
+        display.text = stringResult
+    }
+
+    fun differenceEvaluation() {
+        var expression = display.getText().toString()
+        var numList = expression.split("-")
+        var firstNumber = numList.toMutableList().removeAt(0)
+        var secondNumber = numList.toMutableList().removeAt(1)
+        var result = firstNumber.toFloat() - secondNumber.toFloat()
+        var stringResult = result.toString()
+        display.text = stringResult
+    }
+
+    fun modulusEvaluation() {
+        var expression = display.getText().toString()
+        var numList = expression.split("%")
+        var firstNumber = numList.toMutableList().removeAt(0)
+        var secondNumber = numList.toMutableList().removeAt(1)
+        var result = firstNumber.toFloat() % secondNumber.toFloat()
+        var stringResult = result.toString()
+        display.text = stringResult
     }
 
 
